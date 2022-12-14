0ecbcd6335a88e95dd1e48fea0d54ecef50576ba
ARTURLEDOU <98919700+ARTURLEDOU@users.noreply.github.com>
2022-12-14 14:20:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,21 @@ var nomeConvidado = document.getElementById("inputConvidados").value
 var list = document.getElementById("lista")
 function Add(){
     names.push(nomeConvidado)
-    list.innerHTML = "names"
+    list.innerHTML = names;
+}
+
+function clik(){
+    var s =document.getElementById("s1").value;
+    var found=0;
+    var j;
+    
+    for(j=0; j < names.length; j++){
+        
+        if(s==names[j]){
+            found=found+1;
+        }
+        
+    }
+    
+    document.getElementById("p2").innerHTML="Nome encontrado "+found+" vez(es)";
 }
